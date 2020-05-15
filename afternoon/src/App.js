9be89e057d41cea7b68b3d4d520dafaf914fa0c9
paulbrooks646 <paulbrooks646@gmail.com>
2020-05-15 14:36:03 -0600

    finito

--- afternoon/src/App.js
+++ afternoon/src/App.js
@@ -1,31 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import data from './data.js'
 import './App.css'
-import Comp1 from'./Comp1.js'
+import Comp1 from './Comp1.js'
 import Comp2 from './Comp2.js'
 
 export default class App extends Component {
-constructor() {
-  super()
+  constructor() {
+    super()
 
-  this.state = {
-    users: data
-  }
-
+    this.state = {
+      users: data,
+      ind: 0,
+      num: 1
     }
+this.handle = this.handle.bind(this)
+this.handle2 = this.handle2.bind(this)
+  }
+  handle() {
+    if (this.state.ind === 24) {
+      this.setState({ ind: 0})
+    }
+    else {
+    this.setState({ ind: this.state.ind + 1 })
+  }
   
-render() {
-  
-
-  return  (  
-  
-    <div>
-    <Comp2/>
-  
-    <Comp1 user={this.state.users}/>    
-    </div>
-  
-  )
 }
+  handle2() {
+    if (this.state.ind === 0) {
+      this.setState({ ind: 24})
+    }
+    else {
+    this.setState({ ind: this.state.ind - 1 })
+  }}
+  render() {
+
+
+    return (
+
+      <div>
+        <Comp2 />
+
+        <Comp1 user={this.state.users} ind={this.state.ind} handle={this.handle} handle2={this.handle2}/>
+      </div>
+
+    )
+  }
 
 }
--- afternoon/src/Comp1.js
+++ afternoon/src/Comp1.js
@@ -6,44 +6,41 @@ import data from "./data.js"
 
 function Comp1(props) {
 
-    console.log(props.user[0].favoriteMovies[2])
-    
-    
-function handle() {
-    console.log("Pig")
-} 
-    
+ let {ind, handle, handle2} = props
+ console.log(ind)   
     return (
 <div className="main">
     <div className="center">
         <div className="card">
         <div className="page">
-            <h1>{props.user[0].name.first} {props.user[0].name.last}</h1>
-            <h4>1/25</h4>
+            <h1>{props.user[ind].name.first} {props.user[ind].name.last}</h1>
+            <h4>{ind + 1}/25</h4>
         </div>
         <div className="ciudad">
             <h3>From:</h3>
-            <p>{props.user[0].city}, {props.user[0].country}</p>
+            <p>{props.user[ind].city}, {props.user[ind].country}</p>
         </div>
         <div className="trabajo">
-            <h3>Job Title:</h3> <p>{props.user[0].title}</p>
+            <h3>Job Title:</h3> <p>{props.user[ind].title}</p>
         </div>
         <div className="empleado">
             <h3>Employer:</h3>
-            <p>{props.user[0].employer}</p>
+            <p>{props.user[ind].employer}</p>
         </div>
         <div >
             <h3 className="pelicula">Favorite Movies:</h3>
             <ol>
-                <li>{props.user[0].favoriteMovies[0]}</li>
-                <li>{props.user[0].favoriteMovies[1]}</li>
-                <li>{props.user[0].favoriteMovies[2]}</li>
+                <li>{props.user[ind].favoriteMovies[0]}</li>
+                <li>{props.user[ind].favoriteMovies[1]}</li>
+                <li>{props.user[ind].favoriteMovies[2]}</li>
             </ol>
         </div>
         </div>
         <div className="next">
-        <button className="pre" onClick={handle()}>{"<"} Previous</button>
-        <button className="post">Next {">"}</button></div></div></div>)
+        <button className="pre" onClick={handle2}>{"<"} Previous</button>
+        <button className="post" onClick={handle}>Next {">"}</button></div>
+    </div>
+</div>)
 }
 
 export default Comp1
